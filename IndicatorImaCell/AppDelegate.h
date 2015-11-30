72893ef04f1531e4b99fc747d978f38634a142d9
LiuLongChang <1040719208@qq.com>
2015-11-30 10:31:01 +0800

    Initial Commit

---
+++ IndicatorImaCell/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  IndicatorImaCell
+//
+//  Created by langyue on 15/11/30.
+//  Copyright © 2015年 langyue. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ IndicatorImaCell/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  IndicatorImaCell
+//
+//  Created by langyue on 15/11/30.
+//  Copyright © 2015年 langyue. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
